c46cb7894a6d7bc99e493de90eb1b79ac268ae32
Felipe Scrochio Custódio <felipe.crochi@gmail.com>
2018-05-21 20:29:50 -0300

    Colored output, hello world

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 import psycopg2
 import eel
+from termcolor import colored
 # https://github.com/ChrisKnott/Eel
 # http://www.postgresqltutorial.com/postgresql-python/
 
@@ -12,8 +13,23 @@ eel.init('gui')
 
 @eel.expose
 def hello_world():
-    print("Hello, world!")
-    return ("Hello, world!")
+    text = colored('HELLO, WORLD! ', 'yellow', attrs=['reverse', 'blink'])
+    print(text)
 
 
+text = colored('Databases', 'red', attrs=['reverse', 'blink'])
+print(text)
+
+commands = ["INSERT", "SELECT", "ALTER", "DROP"]
+text = colored('QUERY: ', 'red', attrs=['reverse', 'blink'])
+query = "INSERT INTO JOGA (TIME1, TIME2, CLASSICO) VALUES ('PALMEIRAS', 'palmeiras', 1)"
+
+for word in query.split():
+    if (word in commands):
+        word = colored(word, 'yellow', attrs=['reverse', 'blink'])
+    print(word, end=' ')
+print("")
+
+
+print("Initializing GUI...")
 eel.start('index.html', size=(800, 800))

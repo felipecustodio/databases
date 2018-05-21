16988d0601294c7d0e990eb2e9f5ca43ff9b54e0
Felipe Scrochio Custódio <felipe.crochi@gmail.com>
2018-05-21 20:29:19 -0300

    Check if packages are installed

--- initialize.sh
+++ initialize.sh
@@ -1,3 +1,12 @@
 # initialize postgresql (windows/linux)
+
+
+# check if packages are installed
+if python -c "import package_name" &> /dev/null; then
+    echo 'Packages installed.'
+else
+    echo 'Install packages first using pip install -r requirements.txt'
+fi
+
 # run python script
-# open frontend
+python main.py
